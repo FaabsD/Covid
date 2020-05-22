c98865bc7ac80a0f06962529e39a79d0dda6db6a
Fabian Hendriks <19959@ma-web.nl>
2020-05-22 15:56:11 +0200

    Update register.php
    De registratie pagina maakt nu gebruik van de website.php layout, kunnen wij later nog wat aanpassen

---
+++ private/views/register.php
@@ -0,0 +1,16 @@
+<?php $this->layout('website');?>
+<?php $this->start('page_title')?>
+    Registreren
+<?php $this->stop()?>
+<?php $this->start('header_content')?>
+    Registreren
+<?php $this->stop()?>
+<?php $this->start('main_content')?>
+    <form action="" method="post">
+        <input type="email" name="email" placeholder="E-mail adres">
+        <input type="text" name="fullname" placeholder="Volledige naam">
+        <input type="text" name="username" placeholder="Gebruikersnaam">
+        <input type="password" name="password" placeholder="Wachtwoord">
+        <input type="submit" value="Maak account aan">
+    </form>
+<?php $this->stop()?>
