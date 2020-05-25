ec33483737c5f443e60bdff395fdb75e77bf0389
Fabian <19959@ma-web.nl>
2020-05-25 20:31:35 +0200

    (redelijk ver) Bezig met functie om registratie af te handelen

--- private/controllers/RegistrationController.php
+++ private/controllers/RegistrationController.php
@@ -16,6 +16,39 @@
 //		$template_engine = get_template_engine();
 //		echo $template_engine->render('homepage');
 
-	}
+    }
+    public function handleRegistration(){
+        // hier wordt het formulier afgehandeld
+
+        $errors = [];
+        // check op echt e-mailadres
+        $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+        $password = trim($_POST['password']);
+
+        if ( $email === false ){
+            $errors['email'] = "Geen geldig e-mailadres ingevuld";
+        }
+        // check of wachtwoord minstens 6 tekens bevat
+        if ( strlen( $password ) < 6 ){
+            $errors['password'] = "Wachtwoord is te kort (moet minstens 6 tekens bevatten)";
+        }
+
+        if ( count( $errors ) === 0 ) {
+            //Sla gebruiker op
+            //Check of het email al in gebruik is
+            $connection = dbConnect();
+            $sql = "SELECT * FROM users WHERE email = :email";
+            $statement = $connection->prepare( $sql );
+            $statement->execute( [ 'email' => $email ] );
+
+            if ( $statement->rowCount() === 0 ){
+                //geen gebruiker gevonden? Verder met opslaan
+                $sql = "INSERT INTO users"
+            } else{
+                //anders aangeven dat het e-mail al wordt gebruikt
+                $errors['email'] = "Dit e-mailadres is al in gebruik"
+            }
+        }
+    }
     }
 ?>
