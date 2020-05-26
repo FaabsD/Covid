93406521055d7acdf213cfa2a5ac32d1d92b6387
Fabian Hendriks <19959@ma-web.nl>
2020-05-26 13:56:13 +0200

    code voor het tonen van de errors toegepast

--- private/views/register.php
+++ private/views/register.php
@@ -3,12 +3,20 @@
     Registreren
 <?php $this->stop()?>
 <?php $this->start('header_content')?>
-    Registreren
+    <?php if ( isset ( $errors['email'] )): ?>
+            <small><?php echo $errors['email']?></small>
+    <?php endif ?>
+    <?php if ( isset ( $errors['password'] )): ?>
+            <small><?php echo $errors['password']?></small>
+    <?php endif ?>
+    <h2>
+        Registreren
+    </h2>
 <?php $this->stop()?>
 <?php $this->start('main_content')?>
     <form action="<?php echo url("register.handle")?>" method="post">
-        <input type="email" name="email" placeholder="E-mail adres">
-        <input type="text" name="fullname" placeholder="Volledige naam">
+        <input type="email" name="email" value ="<?php echo input('email') ?>"placeholder="E-mail adres">
+        <input type="text" name="fullname" value="<?php echo input('fullname')?>"placeholder="Volledige naam">
         <input type="text" name="username" placeholder="Gebruikersnaam">
         <input type="password" name="password" placeholder="Wachtwoord">
         <input type="submit" value="Maak account aan">
