ff469e71507058f0b7305eab235897d5d994d6e8
Fabian <19959@ma-web.nl>
2020-05-29 14:48:56 +0200

    voor de bedankpagina de zelfde layout gebruikt als de registratie pagina, moet nog kijken voor een afbeelding voor desktop versie

--- private/views/bedankpagina.php
+++ private/views/bedankpagina.php
@@ -1,4 +1,15 @@
-<?php 
-echo "Bedankt voor het registreren."
-
-?>
+<?php $this->layout('registreer-inlog'); ?>
+<?php $this->start('page_title') ?>
+Bedankt
+<?php $this->stop() ?>
+<?php $this->start('header_content') ?>
+<h2>Bedankt Voor je Registratie</h2>
+<?php $this->stop() ?>
+<?php $this->start('aside_content') ?>
+hchgcgfjgfgfcg
+<?php $this->stop() ?>
+<?php $this->start('main_content') ?>
+Check je E-mail om je account te bevestigen<br>
+<br>
+<a href="<?php echo url('home')?>">Ga terug naar startpagina</a>
+<?php $this->stop() ?>
