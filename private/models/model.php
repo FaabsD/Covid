b908df454a78ddc6630fc4bbe278e1ebb45beb25
Fabian <19959@ma-web.nl>
2020-06-02 21:52:17 +0200

    functie aangemaakt die het account bevestigt en de code die bij een gebruiker is aangemaakt weghaald (geen code in database = bevestigd)

--- private/models/model.php
+++ private/models/model.php
@@ -1,3 +1,16 @@
 <?php
 // Model functions
 // In dit bestand zet je ALLE functions die iets met data of de database doen
+
+function getUserByCode($code){
+    $connection = dbConnect();
+    $sql = "SELECT * FROM `users` WHERE `code` = :code";
+    $statement = $connection->prepare($sql);
+
+    $statement->execute(['code' => $code]);
+
+    if ( $statement->rowCount() === 1 ) {
+        return $statement->fetch();
+    }
+    return false;
+}
