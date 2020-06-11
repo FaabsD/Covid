13a2327e84e05ffb5766d4ee3c7ce53e10b3de48
Fabian <19959@ma-web.nl>
2020-06-11 23:01:01 +0200

    Create winkel-toevoegen.php

---
+++ private/views/winkel-toevoegen.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Homepage</title>
+    <link rel="stylesheet" href="<?php echo site_url('/css/style.css') ?>">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <div class="sidebar">
+        <div class="logo">
+            <img src="<?php echo site_url('/images/binoculars.png') ?>" alt="binoculars">
+            <h2>Druktezoeker</h2>
+        </div>
+        <nav>
+            <ul>
+                <li>
+                    <div class="content-active">
+                        <img src="<?php echo site_url('/images/home.png') ?>" alt="home">
+                        Home
+                    </div>
+                </li>
+                <li>
+                    <div class="content">
+                        <img src="<?php echo site_url('/images/fire.png') ?>" alt="favorieten">
+                        Favorieten
+                    </div>
+                </li>
+                <li>
+                    <div class="content">
+                        <img src="<?php echo site_url('/images/verified.png') ?>" alt="overons">
+                        Over Ons
+                    </div>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    <main>
+        <div class="header">
+            <div class="searchbar">
+                <form class="search" method="post" action="<?php echo url('zoekresultaten') ?>" style="max-width:200px">
+                    <button type="submit"><i class="fa fa-search"></i></button>
+                    <input type="text" placeholder="Zoeken.." name="search">
+                </form>
+            </div>
+        </div>
+        <div class="content">
+            <section class="left">
+                <div class="intro">
+                    <h1>Winkel toevoegen</h1>
+                    <p>Staat uw winkel er niet tussen? Via het formulier kunt u deze zelf toevoegen.</p>
+                </div>
+                <div class="populair">
+                    <h3>Drukte bij: </h3>
+                    <form action="<?php echo url('handle.winkel') ?>" method="post">
+                        <label for="winkelnaam">Winkel:</label>
+                        <input type="text" name="winkelnaam" placeholder="bijv. Dekamarkt"><br>
+                        <label for="adres">Adres:</label>
+                        <input type="text" name="adres" placeholder="bijv. Drielse Wetering 48"><br>
+                        <label for="plaats">Plaats:</label>
+                        <input type="text" name="plaats" placeholder="bijv. Zaandam"><br>
+                        <label for="drukte">Drukte</label>
+                        <select name="drukte">
+                            <option value="Niet druk">Niet druk</option>
+                            <option value="Druk">Druk</option>
+                            <option value="Heel druk">Heel druk</option>
+                        </select><br>
+                        <input type="submit" value="Voeg toe">
+                    </form>
+                </div>
+
+            </section>
+            <section class="right">
+                <h1>hoi</h1>
+            </section>
+        </div>
+
+    </main>
+
+
+</body>
+
+</html>
