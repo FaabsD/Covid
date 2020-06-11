c39a86c019f28e4b674f391ac4d43d5f94f97f3b
Fabian <19959@ma-web.nl>
2020-06-11 18:13:03 +0200

    een fout opgelost

--- private/views/drukte.php
+++ private/views/drukte.php
@@ -57,10 +57,12 @@
                 <div class="populair">
                     <h3>Drukte bij: </h3>
                     <!-- geef een formulier mits doorvewezen vanaf resultaten pagina -->
-                    <?php if(isset ($winkel)): ?>
-                        <form action="<?php echo url('drukte.handle')?>" method="post">
-                            <legend><?php foreach($winkel as $gegevens){echo $gegevens['winkelnaam'] . ' ' . $gegevens['adres'] . ' ' . $gegevens['plaats'];}?></legend>
-                            <input type="hidden" name="id" value="<?php echo $gegevens['id']?>">
+                    <?php if (isset($winkel)) : ?>
+                        <form action="<?php echo url('drukte.handle') ?>" method="post">
+                            <legend><?php foreach ($winkel as $gegevens) {
+                                        echo $gegevens['winkelnaam'] . ' ' . $gegevens['adres'] . ' ' . $gegevens['plaats'];
+                                    } ?></legend>
+                            <input type="hidden" name="id" value="<?php echo $gegevens['id'] ?>">
                             <select name="drukte" id="">
                                 <option value="Niet druk">Niet druk</option>
                                 <option value="Druk">Druk</option>
@@ -68,17 +70,14 @@
                             </select>
                             <input type="submit" value="Wijzig">
                         </form>
-                    <?php endif ?>
-                    <!-- geef een bevestiging als de drukte is aangepast -->
-                    <?php if (isset($bevestiging)): ?>
-                        <?php echo $bevestiging ?>
-                        <br><a href="<?php echo url('home')?>">Ga terug naar Homepage</a>
-                    <!-- niet doorverwezen geef een melding -->
-                    <?php else:?>
-                        <?php echo 'Er is geen winkel geselecteerd' ?>
-                    <?php endif ?>
-
-
+                        <!-- geef een bevestiging als de drukte is aangepast -->
+                        <?php elseif (isset($bevestiging)) : ?>
+                            <?php echo $bevestiging ?>
+                            <br><a href="<?php echo url('home') ?>">Ga terug naar Homepage</a>
+                        <!-- niet doorverwezen geef een melding -->
+                        <?php else : ?>
+                            <?php echo 'Er is geen winkel geselecteerd' ?>
+                        <?php endif ?>
                 </div>
 
             </section>
