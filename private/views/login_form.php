0d82ec389cce0ea6227bc73feb94b658ed3fac81
Fabian <19959@ma-web.nl>
2020-06-14 20:22:30 +0200

    laat login page gebruyik maken van layout

--- private/views/login_form.php
+++ private/views/login_form.php
@@ -1,27 +1,30 @@
-<?php $this->layout('website');?>
-    <h2>
-        Login
-    </h2>
-
-    <form action="<?php echo url( 'login.handle' ) ?>"method="POST">
+<?php $this->layout('registreer-inlog'); ?>
+<?php $this->start('page_title') ?>
+Inloggen
+<?php $this->stop() ?>
+<?php $this->start('header_content'); ?>
+<h2>
+    Login
+</h2>
+<?php $this->stop(); ?>
+<?php $this->start('aside_content') ?>
+<img src="<?php echo site_url('/images/Icon awesome-city.png') ?>" alt="Stad illustratie">
+<?php $this->stop(); ?>
+<?php $this->start('main_content') ?>
+<form action="<?php echo url('login.handle') ?>" method="POST">
     <div class="form-group">
-    <label for="email">Email</label>
-    <input type="email" name="email" value ="<?php echo input('email') ?>"placeholder="E-mail adres">
-    <?php if ( isset( $errors['email'] ) ): ?>
-        <?php echo $errors['email'] ?>
-    <?php endif; ?>
+        <input type="email" name="email" value="<?php echo input('email') ?>" placeholder="E-mail adres">
+        <?php if (isset($errors['email'])) : ?>
+            <?php echo $errors['email'] ?>
+        <?php endif; ?>
     </div>
     <div class="form-group">
-        <label for="wachtwoord">Wachtwoord</label>
-        <input type="password" name="wachtwoord" class="form-control" id="wachtwoord">
-        <?php if ( isset( $errors['wachtwoord'] ) ): ?>
+        <input type="password" name="wachtwoord" class="form-control" id="wachtwoord" placeholder="Wachtwoord">
+        <?php if (isset($errors['wachtwoord'])) : ?>
             <?php echo $errors['wachtwoord'] ?>
         <?php endif; ?>
     </div>
 
-    <butoon type="submit" class="btn btn-primary">inloggen</button>
-    </form>
-
-
-        <img src="<?php echo site_url('/images/Icon awesome-city.png')?>" alt="Stad illustratie">
-
+    <input type="submit" class="btn btn-primary" value="inloggen">
+</form>
+<?php $this->stop() ?>
