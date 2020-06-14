5ed25c5fce78fc492b43c47f96f9ff08d245d0d5
BramvanBaren <55235762+BramvanBaren@users.noreply.github.com>
2020-06-14 12:48:20 +0200

    Add files via upload

---
+++ private/controllers/logincontroller.php
@@ -0,0 +1,41 @@
+<?php
+
+    namespace Website\Controllers;
+
+use function DI\create;
+
+
+    class loginController {
+    	public function loginForm() {
+
+		$template_engine = get_template_engine();
+		echo $template_engine->render('login_form');
+ }
+
+    public function handleLoginForm(){
+       $result = validateRegistrationData($_POST);
+
+       if(userNotRegistered( $result['data']['email'] ) ) {
+        $result['errors']['email'] = 'deze gebruiker is nog niet geregistreerd';
+       } else{
+        $user = getUserByEmail( $result['data']['email'] );
+        if(password_verify($result['data']['wachtwoord'], $user['wachtwoord'])){
+
+            $_SESSION['user_id'] =$user['id'];
+
+            redirect(url('user.dashboard'));
+
+        }else{
+            $result['errors']['wachtwoord'] = 'Wachtwoord is incorrect';
+        }
+       }
+
+       $template_engine = get_template_engine();
+       echo $template_engine->render( 'login_form', ['errors' => $result['errors']] );
+    }
+
+    public function userDashboard(){
+
+        echo "Je ben ingelogd";
+    }
+}
