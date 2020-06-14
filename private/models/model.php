22bf88c997aa9cd34a4feb24a870bdb552fdeef0
BramvanBaren <55235762+BramvanBaren@users.noreply.github.com>
2020-06-14 12:52:15 +0200

    Update model.php

--- private/models/model.php
+++ private/models/model.php
@@ -2,6 +2,26 @@
 // Model functions
 // In dit bestand zet je ALLE functions die iets met data of de database doen
 
+function getUsers(){
+    $connection = dbConnect();
+    $sql        = "SELECT * FROM 'users'";
+    $statement  = $connection->query( $sql );
+
+    return $statement->fetchAll();
+}
+
+function getUserByEmail($email){
+    $connection = dbConnect();
+    $sql        = "SELECT * FROM `gebruikers` WHERE email = :email";
+    $statement  = $connection->prepare( $sql );
+    $statement->execute( [ 'email' => $email ] );
+
+    if($statement->rowCount() === 1 ){
+        return $statement-fetch();
+    }
+ return false;
+}
+
 //zoek een gebruiker bij de code
 function getUserByCode($code){
     $connection = dbConnect();
